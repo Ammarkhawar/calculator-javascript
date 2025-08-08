18419b69e5e3a8d5558533c2b14409dea497c63d
Ammar Khawar <ammarkhawar@Ammars-MacBook-Pro.local>
2025-08-09 03:06:52 +0500

    Added the Logic to make the Calculator Faulty

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ let buttons = document.querySelectorAll('.input-row-child');
 let history = document.getElementById('history');
 let result = document.getElementById('result');
 
+let obj = {
+    '+' : '-',
+    '*' : '+',
+    '-' : '/',
+    '/' : '**'
+}
+
 let expression = '';
 
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) => {
 
         if (e.target.dataset.value == '=') {
-            document.getElementById('history').innerHTML = expression; 
-            expression = eval(expression).toString();
-            document.getElementById('result').innerHTML = expression;
+            document.getElementById('history').innerHTML = expression;
+            let random = Math.random();
+            console.log(random);
+            if (random > 0.5) { 
+                expression = eval(expression).toString();
+                document.getElementById('result').innerHTML = expression;
+            }
+            else{
+                let tempExpression = expression;
+                const matchOperator = tempExpression.match(/[*/+\-]/);
+                tempExpression = tempExpression.slice(0, matchOperator.index) + obj[matchOperator[0]] + tempExpression.slice(matchOperator.index + 1);
+                tempExpression = eval(tempExpression).toString();             
+                document.getElementById('result').innerHTML = tempExpression; 
+            }
+            
             
         }
         else if (e.target.dataset.value == "C"){
